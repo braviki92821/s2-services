016f4a1617da3ab5719c1efadf7117d46e7098fc
braviki92821 <braviki92821@gmail.com>
2023-02-28 18:41:12 -0600

    "otra modificacion a la clase users"

--- s2-services/Models/users.cs
+++ s2-services/Models/users.cs
@@ -11,6 +11,7 @@ namespace s2_services.models
         private string username;
         [BsonElement]
         private string password;
+        [BsonElement]
         private string[] scope;
 
         public users() { }
@@ -18,6 +19,7 @@ namespace s2_services.models
         public string Username { get => username; set => username = value; }
         public string Password { get => password; set => password = value; }
         public ObjectId Id { get => id; set => id = value; }
+        public string[] Scope { get => scope; set => scope = value; }
         //public string[] Scope { get => scope; set => scope = value; }
 
     }
